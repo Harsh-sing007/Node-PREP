3e9f06abfb1699fe6a3ee98094f6cd5bc1704af3
Harsh <harshsingh45831@gmail.com>
2026-02-19 14:49:32 +0530

    update

--- index6.js
+++ index6.js
@@ -1,4 +1,16 @@
 import express from 'express';
 const app = express();
 app.listen(8080);
-app.get("/")
+// app.get("/:name",(req,res)=> {
+//     res.send("Hello "+req.params.name);                     //IN postman we used   http://localhost:8080/john to get Hello John
+// });                                                         //earlier we used query string like http://localhost:8080/?name=john
+
+// app.get("/",(req,res)=> {
+//     res.send("Hello "+req.query.name);                    
+// });                                                            //    http://localhost:8080/?name=john
+
+//status 200 means success. If we dont write res.send() it will stuck in infinite loop
+
+app.get("/",(req,res)=> {
+    res.send("Hello "+req.query.name + req.query.age);                       //   http://localhost:8080/?name=john&age=21
+});         
