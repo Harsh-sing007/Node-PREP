aac31dd8f9112fadd7376649b9cb0053279ccbdc
Harsh <harshsingh45831@gmail.com>
2026-02-20 16:46:15 +0530

    update

--- index9a.js
+++ index9a.js
@@ -39,21 +39,47 @@
 
 
 //get method, auth, bearer token  
-import express from 'express';
-const app = express();
-app.listen(8080, () => {
-    console.log("Server running on port 8080");
-});
-app.get("/", (req, res) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send("No Token Provided");
-    }
-    const token = authHeader.split(" ")[1];
+// import express from 'express';
+// const app = express();
+// app.listen(8080, () => {
+//     console.log("Server running on port 8080");
+// });
+// app.get("/", (req, res) => {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send("No Token Provided");
+//     }
+//     const token = authHeader.split(" ")[1];
 
-    if (token === "1234") {
-        return res.status(200).send("Welcome");
-    } else {
-        return res.status(403).send("Access Denied");
-    }
+//     if (token === "1234") {
+//         return res.status(200).send("Welcome");
+//     } else {
+//         return res.status(403).send("Access Denied");
+//     }
+// });
+
+//login
+import express from "express";
+const app = express();
+const token = Math.round(Math.random() * 100000 ).toString()
+app.use(express.json());
+
+const auth = (req, res, next) => {
+    const val = req.headers.authorization
+    const tokenValue = val.split(" ")
+    if(tokenValue[1] === token) next()
+    else res.send("unauthrized")
+};
+
+app.get("/", auth,(req, res) => {
+    res.send("Welcome");
+});
+
+app.post("/login", (req, res) => {
+    res.send(token)
+})
+
+
+app.listen(8080, () => {
+  console.log("server is live");
 });
