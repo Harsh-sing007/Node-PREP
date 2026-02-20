5d652b08f5a755e3e0552d6dc2f930becbf79995
Harsh <harshsingh45831@gmail.com>
2026-02-20 15:50:09 +0530

    task

--- index9a.js
+++ index9a.js
@@ -1,18 +1,38 @@
+// import express from 'express';
+// const app = express();
+
+// app.listen(8080, () => {
+//     console.log("Server running on port 8080");
+// });
+// const auth = (req, res, next) => {
+//     const id = req.params.id;
+
+//     if (id === "1234") {
+//         next(); 
+//     } else {
+//         res.send("Access Denied");
+//     }
+// };
+// app.get("/:id", auth, (req, res) => {
+//     res.send("Welcome");
+// });
+
+
+// when a json  token format is given 
 import express from 'express';
 const app = express();
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
 });
-const auth = (req, res, next) => {
-    const id = req.params.id;
+app.use(express.json());
 
-    if (id === "1234") {
-        next(); 
+app.post("/", (req, res) => {
+    const token = req.body.token;
+
+    if (token === "1234") {
+        res.status(200).send("Welcome");
     } else {
-        res.send("Access Denied");
+        res.status(403).send("Access Denied");
     }
-};
-app.get("/:id", auth, (req, res) => {
-    res.send("Welcome");
 });
