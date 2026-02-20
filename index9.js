69a2b95bf0b0e09c197022d508451ca5412e63e2
Harsh <harshsingh45831@gmail.com>
2026-02-20 11:45:04 +0530

    update

--- index9.js
+++ index9.js
@@ -1,14 +1,18 @@
 import express from 'express';
 const app = express();
 app.listen(8080);
-const logger = ()=> {
+const logger = (req,res)=> {
+req.message = "This"
+console.log(req.url)
+next()
+};
 
-}
-app.use(logger)
+app.use(logger);
 
 app.get("/",(req,res)=>{
    // res.end()
   // res.send("Hello World")
    // res.json({name:"Johm",age: 21})
+   console.log(req.url);
    res.json(req.url);
 });
