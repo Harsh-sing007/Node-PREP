58dc420e0ff6092ab9a8b0ce63e65359744c9df7
Harsh <harshsingh45831@gmail.com>
2026-02-23 13:36:18 +0530

    jwt

---
+++ index14.js
@@ -0,0 +1,13 @@
+//npm install jsonwebtoken(jwt)
+import express from "express"
+import jwt from "jsonwebtoken"
+const SECRET ="lpu"
+const app = express()
+app.listen(8080,()=>console.log("Server Started"));
+const user = {
+    name:"John",
+    email:"john@gmail.com",
+    role:"student"
+}
+const token = jwt.sign(user,SECRET,{expiresIn:"1hr"})
+console.log(token)
