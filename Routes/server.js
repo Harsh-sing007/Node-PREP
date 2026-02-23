471002dd34afb6df9f6ee520b65a51425a82b003
Harsh <harshsingh45831@gmail.com>
2026-02-23 11:46:40 +0530

    updates

--- Routes/server.js
+++ Routes/server.js
@@ -1,19 +1,6 @@
-//configuration was done in routes folder
-// import express from "express"
-// import userRouter from "./userRoutes.js"
-// import productRouter from "./productRoutes.js"
-
-// const app = express()
-
-// app.listen(8080, () => console.log("Server Started"))
-
-// app.use("/api/users", userRouter)
-// app.use("/api/product", productRouter)
-
-
 import express from "express"
-import userRouter from "./userRouter.js"
-import productRouter from "./productRouter.js"
+import userRouter from "./routers/userRouter.js"
+import productRouter from "./routers/productRouter.js"
 
 const app = express()
 
